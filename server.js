27e86594146e9b980ecdbfcef640f30793fe91ac
artsoroka <githubrepository@yandex.ru>
2015-12-02 01:29:59 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+var Promise = require('bluebird'); 
+var WebTorrent = require('./webtorrent'); 
+var fs = require('fs'); 
+var parser = require('parse-torrent'); 
+
+module.exports = function(config){
+  return new Promise(function(resolve, reject){
+    var file   = parser(fs.readFileSync(config.file));  
+    var magnet = parser.toMagnetURI({
+        infoHash: file.infoHash
+    });
+    var client = new WebTorrent()
+
+    client.download(file, function (torrent) {
+      console.log('Torrent info hash:', torrent.infoHash)
+
+      torrent.on('done', function(){
+        console.log('download is complete'); 
+        resolve({
+          file: config.outputFile, 
+          size: fs.statSync(config.outputFile) 
+        }); 
+      }); 
+
+      torrent.files.forEach(function (file) {
+        // Stream each file to the disk
+        var source = file.createReadStream()
+        var destination = fs.createWriteStream(config.outputFile); 
+        source.pipe(destination)
+      })
+
+    })
+
+  })
+
+}
